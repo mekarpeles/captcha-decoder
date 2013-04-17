a647f7f51bdaa847988aecf88b68ee797f4e4111
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-04-16 23:21:48 -0700

    buildscript

---
+++ decaptcha/setup.py
@@ -0,0 +1,34 @@
+#-*- coding: utf-8 -*-
+
+"""
+    waltz
+    ~~~~~
+
+    Setup
+    `````
+
+    $ pip install waltz    
+"""
+
+from distutils.core import setup
+import os
+
+setup(
+    name='decaptcha',
+    version='0.0.1',
+    url='http://github.com/mekarpeles/captcha-decoder',
+    author='mek',
+    author_email='michael.karpeles@gmail.com',
+    packages=[
+        'decaptcha',
+        ],
+    platforms='any',
+    license='LICENSE',
+    install_requires=[
+    ],
+    scripts=[
+        "scripts/decaptcha"
+        ],
+    description="Basic Captcha Cracker",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+)
